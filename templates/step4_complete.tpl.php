3d1ec9984b4d1a39fcd34303ca6f62b95cd40da6
derchris <derchris@me.com>
2018-10-07 22:57:29 +0200

    modify links

--- templates/step4_complete.tpl.php
+++ templates/step4_complete.tpl.php
@@ -8,7 +8,8 @@ $this->includeAtTemplateBase('includes/header.php'); ?>
 <div style="margin: 1em">
   <!-- <h1><?php echo $this->t('new_complete_head'); ?></h1> -->
   <p><?php echo $this->t('new_complete_para1', $this->data['systemName']); ?></p>
-  <ul>
+  <ul class="lead">
+    <li><a href="https://projects.make-munich.de"><?php echo $this->t('link_project'); ?></a></li>
     <li><a href="login.php"><?php echo $this->t('link_review'); ?></a></li>
     <!-- <li><a href="delUser.php"><?php echo $this->t('link_deluser'); ?></li> -->
   </ul>
